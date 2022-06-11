9d40b6d3bd190fe67131a3d1b5865712850380a0
EugeniaKozhevina <kozhevinaeugenia@gmail.ru>
2022-06-11 23:55:34 +0300

    update file

--- HW_01.js
+++ HW_01.js
@@ -1,7 +1,16 @@
 let yolochka4 = 4;
 let yolochka6 = 6;
+let yolochka_name = "zelyonaya"
 
 let result = yolochka4 + yolochka6;
+let result_2 = yolochka6 + yolochka_name;
+
+console.log(`result -  + ${yolochka4 + yolochka6}`)
 
 console.log(result);
 console.log("Hello");
+
+let t = true
+let f = false
+
+console.log(t + f)
