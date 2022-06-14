634e2a41e868ed118a4e96a2e70bdba451513917
EugeniaKozhevina <106934379+EugeniaKozhevina@users.noreply.github.com>
2022-06-14 23:01:46 +0300

    corrections to file

--- HW_01.js
+++ HW_01.js
@@ -8,12 +8,10 @@ item_1 = 5
 console.log(item_1)
 
     /* 4. Создать переменную “item_2”
-    5. Присвоить переменной item_2 цифру 3 */
+    5. Присвоить переменной item_2 цифру 3
+    6. Вывести в консоль item_2 */
 
 let item_2 = 3
-
-    // 6. Вывести в консоль item_2
-
 console.log(item_2)
 
     /*7. Создать переменную “item_3”
@@ -51,13 +49,13 @@ let item_5 = item_3
 let item_6 = 15
 let item_6_type = typeof item_6
 
-    //21. Вывести в консоль значения данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
+    //21. Вывести в консоль значения данных item_6 в виде “item_6 ==”  item_6,  “item_6_type ==”  item_6_type
 
-console.log('item_6 == ' + item_6 + ", ", 'item_6_type == ' + item_6_type)
+console.log('item_6 ==', item_6,", ",'item_6_type ==', item_6_type)
 
-    //22. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
+    //22. Вывести в консоль тип данных item_6 в виде “item_6 ==”  item_6,  “item_6_type ==”  item_6_type
 
-console.log('item_6 == ' + typeof item_6 + ", ", 'item_6_type == ' + typeof item_6_type)
+console.log('item_6 ==', typeof item_6,", ",'item_6_type ==', + typeof item_6_type)
 
     /* 23. Создать переменную item_7 и в ней преобразовать item_6 в String
     24. Создать переменную item_7_type
@@ -66,17 +64,17 @@ console.log('item_6 == ' + typeof item_6 + ", ", 'item_6_type == ' + typeof item
 let item_7 = String(item_6)
 let item_7_type = typeof item_7
 
-    //26. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——
+    //26. Вывести в консоль тип данных item_7 в виде “item_7 ==”  item_7,  “item_7_type ==”  item_7_type
 
-console.log('item_7 == ' + String(item_6) + ", ", 'item_7_type == ' + typeof item_7)
+console.log('item_7 ==', String(item_6),", ",'item_7_type ==', typeof item_7)
 
     /* 27. Создать переменную “age_1” и присвоить ей значение 10
     28. Создать переменную “age_2” и присвоить ей значение 18
     29. Создать переменную “age_3” и присвоить ей значение 60
     30. Создать if в котором будете проверять значение переменной age_1
-    31. Если age_1 < age_2, вывести в консоль “You don’t have access 'cause your age is ” + age_1 + “ It’s less then ”
+    31. Если age_1 < age_2, вывести в консоль “You don’t have access 'cause your age is ” + age_1 + “ It’s less than ”
     32. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome!”
-    33. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”
+    33. Если age_1  >= age_3, вывести в консоль “Keep calm and watch Culture channel”
     34. Иначе выводите “Technical work” */
 
 let age_1 = 10
@@ -84,11 +82,11 @@ let age_2 = 18
 let age_3 = 60
 
 if (age_1 < age_2) {
-    console.log("You don't have access 'cause your age is " + age_1 + ". It's less then " + age_2)
+    console.log("You don't have access 'cause your age is " + age_1 + ". It's less than " + age_2)
 } else if (age_1 >= age_2 && age_1 < age_3) {
     console.log("Welcome!")
 } else if (age_1 >= age_3) {
-    console.log("Keep calm and look 'Culture channel'")
+    console.log("Keep calm and watch 'Culture channel'")
 } else {
     console.log("Technical work")
 }
